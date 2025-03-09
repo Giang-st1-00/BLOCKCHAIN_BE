9de05223dbf86e6a7b505a24d74bb6599f168c47
Giang-st1-00 <bin17022003@gmail.com>
2025-03-09 10:26:21 +0700

    feat: set credentials false

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -2,7 +2,7 @@ import { HttpStatus } from '@nestjs/common';
 import { env } from '~config/env.config';
 
 export const corsConfig = {
-    origin: '*',
+    origin: env.CORS.ALLOWED_ORIGINS,
     allowedHeaders: 'Content-Type, Authorization',
     exposedHeaders: 'Content-Length, X-Knowledge-Base',
     credentials: false,
