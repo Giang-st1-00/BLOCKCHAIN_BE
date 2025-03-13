3725a5c063f1dd0e132617671fc95ed13a3c3a7a
Giang-st1-00 <bin17022003@gmail.com>
2025-03-14 01:56:38 +0700

    feat: certificate

--- src/certificates/services/certificates.service.ts
+++ src/certificates/services/certificates.service.ts
@@ -140,7 +140,7 @@ export class CertificateService {
   }
 
   async getTeacherCertificateTypeDetail(certificateTypeId: string) {
-    const result: any = await this.userCertificateTypeRepo.find({
+    const result: any = await this.userCertificateTypeRepo.findOne({
       where: { certificateTypeId },
     });
   
